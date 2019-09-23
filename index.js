2b96535cb9cdc88df58ec99dc074663d966803aa
Adham  Hamed <hameda6256@gmail.com>
2019-09-23 08:17:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function dwarfRollCall(dwarves) {
-}
+var dwarves = ["Doc", "Dopey", "Grmpuy", "Bashfull"];
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+var words = ["earth", "wind", "fire", "water", "heart"]
+var foods = ["gouda", "chedar", "crackers"]
 
+function dwarfRollCall(dwarves) {
+  var newDwarfs = [];
+  for (var i = 0; i < dwarves.length; i++){
+    newDwarfs.push(`${i + 1}. ${dwarves[i]} `);
+  }
+  return newDwarfs.join("");
+}
 function summonCaptainPlanet(planeteerCalls){
+  var array = [];
+  for (var i=0; i<planeteerCalls.length; i++){
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase(); 
+    array.push(`${planeteerCalls[i]}!`);
+  }
+  return array;
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length<=4){
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i=0; i<foods.length; i++){
+    foods.shift();
+    if (foods[i] === "gouda"){
+      return `gouda`;
+    }
+    else if (foods[i] === `cheddar`){
+      return 'cheddar';
+    }
+    else {
+      return `no cheese!`;
+      }
+  }
 }
+
